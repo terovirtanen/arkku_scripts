f8a27f894485ffb7a5f484f9da91066b62332ef0
Tero Virtanen <tero.tapio.virtanen@gmail.com>
2025-08-15 21:20:08 +0300

    porssisahko data paivitys

--- root/htdocs/kannattaako_porssisahko.php
+++ root/htdocs/kannattaako_porssisahko.php
@@ -39,7 +39,7 @@
                     Voit myös antaa .csv tiedoston jossa on kahdessa sarakkeessa päivämäärä ja kulutus kWh. Tiedostossa tulee olla päivämäärä ja kulutus erotettuna puolipisteellä ja rivit rivinvaihdolla. 
                     Tiedostoja ei tallenneta mihinkään vaan selain lukee tiedoston ja tiedot lasketaan selaimessa.</p>
                     <p>Pörssisähkön tuntikohtaiset hintatiedot on haettu <a href="https://porssisahko.net/">porssisahko.net</a> sivuilta. 
-                    Tuntikohtaiset hintatiedot on vuodesta 2021 alkaen ja viimeisin 31.1.2025. Tällä aikavälillä voidaan siis laskenta suorittaa.</p>
+                    Tuntikohtaiset hintatiedot on vuodesta 2021 alkaen ja viimeisin 16.8.2025. Tällä aikavälillä voidaan siis laskenta suorittaa.</p>
                     <p>Tuntikohtaisista tiedoista lasketaan kuukausi ja vuosikohtaiset kulutus ja hintatiedot pörssisähköllä.</p>
                     <p>
                     Hyväksytyt tiedostomuodot: .xlsx, .csv<br>
