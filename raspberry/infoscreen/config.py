aa4655fb61ae1696525d3d7d12f81bdc37fb6336
Tero Virtanen <tero.tapio.virtanen@gmail.com>
2026-01-17 19:59:25 +0200

    infoscreen initial

---
+++ raspberry/infoscreen/config.py
@@ -0,0 +1,10 @@
+"""
+WiFi Configuration
+"""
+
+# WiFi credentials
+WLAN_SSID = 'your_wifi_ssid'
+WLAN_PASSWORD = 'your_wifi_password'
+
+
+DEBUG = True
---
+++ raspberry/infoscreen/simple_test.py
@@ -0,0 +1,6 @@
+# https://www.waveshare.com/wiki/Pico-ePaper-7.5-B
+
+# https://github.com/waveshareteam/Pico_ePaper_Code/blob/main/python/Pico-ePaper-7.5-B.py
+
+# text, line, rectangle, circle, ellipse, polygon, arc
+# https://docs.micropython.org/en/latest/library/framebuf.html
